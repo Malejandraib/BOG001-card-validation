04990c413a154689694aa37699a0d48ef22a6d71
Malejandraib <mariaibanez07@gmail.com>
2020-06-17 19:04:42 -0500

    agrega valor a la tarjeta

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import validator from './validator.js';
 
-
 // Seleccionar formulario y guardarlo en una variable
 var paymentForm = document.querySelector('#payment-form');
 
@@ -26,18 +25,25 @@ function onSubmit(event) {
  * el atributo value
  */
   var numberInput = document.querySelector('#tc-number').value;
-  var var1=document.getElementsByClassName('verif')
-  var var2=document.getElementsByClassName('num') //span de html
+  var var1=document.querySelector('.verif')
+  var var2=document.querySelector('.num') //span de html
+  var holder = document.querySelector('.holder-name').value;
+  var var3 = document.querySelector('.name-holder')
+
+  var date = document.querySelector('.date').value;
+  var var4 = document.querySelector('.expiration-date')
+
   
-  var1.textContent='this card number seems to be', validator.isValid(numberInput);
+  var1.textContent ='This card number seems to be ' + validator.isValid(numberInput) + '!';
+  var1.style.display="block";
   
-  console.log(var1)
+  
   var2.textContent = validator.maskify(numberInput) ;
-  console.log(var2)
 
+  var3.textContent= holder;
+  var4.textContent=date;
 
 
   
- //alert("Here is the last four number of your card number "+ validator.maskify(numberInput));
-  alert("This card number seems to be "+ validator.isValid(numberInput));
+  
 } 
