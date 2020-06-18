625ef161d36d81c8e2dfc426f4c239572d9a7527
Malejandraib <mariaibanez07@gmail.com>
2020-06-17 19:12:13 -0500

    comentarios al código

--- src/index.js
+++ src/index.js
@@ -25,25 +25,27 @@ function onSubmit(event) {
  * el atributo value
  */
   var numberInput = document.querySelector('#tc-number').value;
+  //Se obtienen los elementos vacios de enmascarado y donde verifica el valor
   var var1=document.querySelector('.verif')
-  var var2=document.querySelector('.num') //span de html
+  var var2=document.querySelector('.num') //div de html
+  //Se obtiene el valor y el elemento donde esta y estará el holder name
   var holder = document.querySelector('.holder-name').value;
   var var3 = document.querySelector('.name-holder')
-
+  //Se obtiene el valor y el elemento donde esta y estará el la fecha de vencimiento
   var date = document.querySelector('.date').value;
   var var4 = document.querySelector('.expiration-date')
 
-  
+  //Se asigna el valor de is valid a el elemento var 1 y se muestra el display
   var1.textContent ='This card number seems to be ' + validator.isValid(numberInput) + '!';
   var1.style.display="block";
   
-  
+  //Se asigna el valor de maskify a el elemento var 2 
   var2.textContent = validator.maskify(numberInput) ;
 
+  //Se asigna el valor de holder
   var3.textContent= holder;
+  //Se asigna el valor de date
   var4.textContent=date;
 
-
-  
   
 } 
